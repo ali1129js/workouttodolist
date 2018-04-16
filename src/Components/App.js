f730255f43f49f7b2b3086dd7d6f708564858ba4
ali1129js <ali1129js@gmail.com>
2018-04-16 21:41:18 +0200

    Skeleton of the app completed

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,17 +2,19 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-16T21:18:09+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-16T21:27:28+02:00
+ * @Last modified time: 2018-04-16T21:38:35+02:00
  */
 import React, { Component, Fragment } from 'react'
   // import Header from './Layouts/Header'
   // import Footer from './Layouts/Footer'
 import { Header, Footer} from './Layouts'
+import Exercises from './Exercises'
 class App extends Component {
   render() {
     return (
     <Fragment>
       <Header />
+      <Exercises />
       <Footer />
     </Fragment>
     )
---
+++ src/Components/Exercises/index.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: Ali Ismail
+ * @Date:   2018-04-16T21:35:16+02:00
+ * @Last modified by:   Ali Ismail
+ * @Last modified time: 2018-04-16T21:37:00+02:00
+ */
+import React from 'react'
+
+export default props =>
+<div>  <h1>  Hi from exercises</h1></div>
