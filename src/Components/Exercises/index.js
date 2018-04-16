3d106d1de3a5a0219b3c5fc180e611cbbbf4832b
ali1129js <ali1129js@gmail.com>
2018-04-17 00:04:14 +0200

    improved skeleton

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -2,9 +2,15 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-16T21:35:16+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-16T21:37:00+02:00
+ * @Last modified time: 2018-04-16T23:59:38+02:00
  */
 import React from 'react'
-
+import { Grid, Paper } from 'material-ui'
+const style = {
+  Paper : {padding: 20,marginTop: 10,marginBottom: 10}
+}
 export default props =>
-<div>  <h1>  Hi from exercises</h1></div>
+<Grid container>
+  <Grid item sm> <Paper style={style.Paper}> Left </Paper> </Grid>
+  <Grid item sm> <Paper style={style.Paper}> Right </Paper> </Grid>
+</Grid>
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -2,14 +2,14 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-16T21:20:06+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-16T23:46:46+02:00
+ * @Last modified time: 2018-04-16T23:59:56+02:00
  */
 import React from 'react'
 import { AppBar, Toolbar, Typography } from 'material-ui'
 export default props =>
- <AppBar position="static" color="default">
+ <AppBar position="static" color="secondary">
     <Toolbar>
-        <Typography>
+        <Typography color="inherit">
           Exercises Database
         </Typography>
     </Toolbar>
