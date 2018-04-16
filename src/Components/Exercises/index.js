23ceab4aba41add34dfbf5e0a55682f821bc18ba
ali1129js <ali1129js@gmail.com>
2018-04-17 00:17:57 +0200

    more structuring

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -2,15 +2,22 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-16T21:35:16+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-16T23:59:38+02:00
+ * @Last modified time: 2018-04-17T00:16:10+02:00
  */
 import React from 'react'
-import { Grid, Paper } from 'material-ui'
-const style = {
-  Paper : {padding: 20,marginTop: 10,marginBottom: 10}
+import { Grid } from 'material-ui'
+import LeftPane from './LeftPane'
+import RightPane from './RightPane'
+
+const styles = {
+  Paper : {padding: 20, marginTop: 10, marginBottom: 10}
 }
 export default props =>
 <Grid container>
-  <Grid item sm> <Paper style={style.Paper}> Left </Paper> </Grid>
-  <Grid item sm> <Paper style={style.Paper}> Right </Paper> </Grid>
+  <Grid item sm>
+    <LeftPane styles={styles} />
+  </Grid>
+  <Grid item sm>
+    <RightPane styles={styles}/>
+  </Grid>
 </Grid>
