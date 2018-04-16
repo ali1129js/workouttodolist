8e55b806bc64404cd506c6fe778820a6e43924f2
ali1129js <ali1129js@gmail.com>
2018-04-16 23:47:40 +0200

    Layout components and appbar

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -2,9 +2,21 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-16T21:20:20+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-16T21:29:27+02:00
+ * @Last modified time: 2018-04-16T23:45:16+02:00
  */
  import React from 'react'
-
+ import { Paper } from 'material-ui'
+ import Tabs, { Tab } from 'material-ui/Tabs'
  export default props =>
- <div>  <h1>  Footer</h1></div>
+ <Paper >
+   <Tabs
+     value={0}
+     indicatorColor="primary"
+     textColor="primary"
+     centered
+   >
+     <Tab label="Item One" />
+     <Tab label="Item Two" />
+     <Tab label="Item Three" />
+   </Tabs>
+ </Paper>
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -2,9 +2,15 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-16T21:20:06+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-16T21:29:19+02:00
+ * @Last modified time: 2018-04-16T23:46:46+02:00
  */
 import React from 'react'
-
+import { AppBar, Toolbar, Typography } from 'material-ui'
 export default props =>
-<div>  <h1>  Header</h1></div>
+ <AppBar position="static" color="default">
+    <Toolbar>
+        <Typography>
+          Exercises Database
+        </Typography>
+    </Toolbar>
+ </AppBar>
