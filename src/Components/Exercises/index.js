8c04ff627be762a1a555a95e2da23cc7c0116c3c
ali1129js <ali1129js@gmail.com>
2018-04-17 13:57:28 +0200

    Typography lists and styling

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -2,13 +2,13 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-16T21:35:16+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-17T13:43:05+02:00
+ * @Last modified time: 2018-04-17T13:52:18+02:00
  */
 import React , { Fragment} from 'react'
 import { Grid, Paper, Typography, List } from 'material-ui'
 import { ListItem, ListItemText} from 'material-ui/List'
 const styles = {
-  Paper : {padding: 20, marginTop: 10, marginBottom: 10}
+  Paper : {padding: 20, marginTop: 10, marginBottom: 10, hight:500, overflow:'auto'}
 }
 export default ({exercises}) =>
 <Grid container>
@@ -22,16 +22,11 @@ export default ({exercises}) =>
             {block}
           </Typography>
           <List components="ul">
-            {exercises.map(({title}) =>
-              <ListItem button>
-                <ListItemText primary={title}/>
+            {exercises.map(({title, id}) =>
+              <ListItem key={id} button>
+                <ListItemText primary={title} />
               </ListItem>
             )}
-
-          <ListItem button>
-            <ListItemText>
-            </ListItemText>
-          </ListItem>
           </List>
         </Fragment>
       )}
@@ -39,7 +34,12 @@ export default ({exercises}) =>
   </Grid>
   <Grid item sm>
     <Paper style={styles.Paper}>
-      Right
+      <Typography variant="display1">
+        Welcome!
+      </Typography>
+      <Typography variant="subheading" style={{marginTop:20}}>
+        Please select from the list on the Left
+      </Typography>
     </Paper>
   </Grid>
 </Grid>
