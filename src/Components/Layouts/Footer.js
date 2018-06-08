91e7b1793cc73be0ce13ce0fe399cdbbf4495841
ali1129js <ali1129js@gmail.com>
2018-06-08 22:34:10 +0200

    tabs work

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -2,7 +2,7 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-16T21:20:20+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-06-05T14:41:27+02:00
+ * @Last modified time: 2018-06-08T17:02:43+02:00
  */
  import React from 'react'
  import Paper from '@material-ui/core/Paper';
@@ -10,16 +10,13 @@
  import Tab from '@material-ui/core/Tab';
 
   export default ({muscles,onSelect, category}) => {
-    const index = category
-    ? muscles.findIndex(group => group === category) + 1
-    : 0
+    const index = category ? muscles.findIndex(group => group === category) + 1 : 0
     const onIndexSelect = (e, index) =>
       onSelect(index === 0 ? '' : muscles[index - 1])
-    
-    return (
+      return (
       <Paper>
         <Tabs
-          value={0}
+          value={index}
           onChange={onIndexSelect}
           indicatorColor="primary"
           textColor="primary"
